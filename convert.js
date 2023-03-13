9e4f5d1a2d2958404bc24a3cd632c84272781895
Mashita Dewi <87978863+mashitaad@users.noreply.github.com>
2023-03-13 17:26:52 +0700

    feat: convert function

---
+++ convert.js
@@ -0,0 +1,12 @@
+function convertToRupiah(angka) {
+    var rupiah = '';    
+    var angkarev = angka.toString().split('').reverse().join('');
+    for(var i = 0; i < angkarev.length; i++) {
+      if(i%3 == 0) {
+        rupiah += angkarev.substr(i,3)+'.';
+      }
+    }
+    var hasil = 'Rp '+rupiah.split('',rupiah.length-1).reverse().join('');
+    return hasil;
+  }
+  
